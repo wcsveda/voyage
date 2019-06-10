32ed26fb2636f22997176827314358db3de1f149
naaspati <naaspati@gmail.com>
2019-06-10 22:15:23 +0530

    minor

--- dist/js/index-browserify.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-Array.of(document.getElementsByClassName("lighten-1"))
-.forEach(c => c)
--- my_dev/css/_configs/postcss-requires-list.js
+++ my_dev/css/_configs/postcss-requires-list.js
@@ -1,10 +1,10 @@
 module.exports = [
 // 'postcss-comment', // https://www.npmjs.com/package/postcss-comment
-'postcss-for', // https://github.com/antyakushev/postcss-for
+// 'postcss-for', // https://github.com/antyakushev/postcss-for
 // 'postcss-each' // https://github.com/outpunk/postcss-each
-'postcss-nested',
+// 'postcss-nested',
 // 'postcss-jsmath'
- 'postcss-utilities',  // https://github.com/ismamz/postcss-utilities
+//  'postcss-utilities',  // https://github.com/ismamz/postcss-utilities
 // 'postcss-mixins'
 // 'postcss-easings',  // https://github.com/postcss/postcss-easings
 // 'postcss-conditionals'
@@ -13,7 +13,7 @@ module.exports = [
 // 'postcss-color-function',  // https://github.com/postcss/postcss-color-function
 // 'postcss-nested-props'
 // 'lost'
-'postcss-css-reset', // https://www.npmjs.com/package/postcss-css-reset
-'autoprefixer',
+// 'postcss-css-reset', // https://www.npmjs.com/package/postcss-css-reset
+// 'autoprefixer',
 ];
 
